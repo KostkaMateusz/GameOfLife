bdedf38e247de270382021179934dc21be608703
Alakeks <mateuszkostka5@gmail.com>
2022-12-02 00:36:28 +0100

    Code optimalization reduce tiome by half

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using GameOfLife;
 using System.Diagnostics;
+using System.Runtime.Serialization.Formatters;
 //Any live cell with two or three live neighbours survives.
 //Any dead cell with three live neighbours becomes a live cell.
 //All other live cells die in the next generation. Similarly, all other dead cells stay dead.
@@ -7,7 +8,7 @@ using System.Diagnostics;
 var stopwatch = new Stopwatch();
 stopwatch.Start();
 
-var game = new GameOfLifeEngine(10, 10);
+var game = new GameOfLifeEngine(6000, 6000);
 
 Cell[,] table = game.table;
 
@@ -19,15 +20,15 @@ table[3, 6].currentValue = true;
 for (int k = 0; k < 5; k++)
 {
     game.CalculateNext();
-    for (int i = 0; i < table.GetLength(0); i++)
-    {
-        for (int j = 0; j < table.GetLength(1); j++)
-        {
-            Console.Write(table[i, j].currentValue.ToString() + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
+    //for (int i = 0; i < table.GetLength(0); i++)
+    //{
+    //    for (int j = 0; j < table.GetLength(1); j++)
+    //    {
+    //        Console.Write(table[i, j].currentValue.ToString() + " ");
+    //    }
+    //    Console.WriteLine();
+    //}
+    //Console.WriteLine();
 }
 
 
@@ -48,37 +49,52 @@ class GameOfLifeEngine
         table = new Cell[sizeX, sizeY];
 
         // initalize objects Cells
-        for (int i = 0; i < table.GetLength(0); i++)
-            for (int j = 0; j < table.GetLength(1); j++)
-                table[i, j] = new Cell(i, j, table.GetLength(0), table.GetLength(1));
+        int height = table.GetLength(0);
+        int width = table.GetLength(1);
+
+        Parallel.For(0, height, y =>
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                table[y, x] = new Cell(y, x, table.GetLength(0), table.GetLength(1));
+
+            }
+        });
+            //    for (int i = 0; i < table.GetLength(0); i++)
+            //for (int j = 0; j < table.GetLength(1); j++)
     }
 
     public Cell[,] CalculateNext()
     {
-        for (int i = 0; i < table.GetLength(0); i++)
-            for (int j = 0; j < table.GetLength(1); j++)
-            {
+        int height = table.GetLength(0);
+        int width = table.GetLength(1);
+
+        Parallel.For(0, height, y =>
+        {
+            for (int x = 0; x < width; ++x)
+            {               
                 int number_of_live_cells = 0;
-                foreach (var neighbourAddress in table[i, j].NeighborsAddresses)
+                foreach (var neighbourAddress in table[y, x].NeighborsAddresses)
                 {
                     var neighbour = table[neighbourAddress.xPosition, neighbourAddress.yPosition];
                     if (neighbour != null && neighbour.currentValue == true)
                         number_of_live_cells++;
                 }
 
-                if (table[i, j].currentValue == true && (number_of_live_cells == 2 || number_of_live_cells == 3))
+                if (table[y, x].currentValue == true && (number_of_live_cells == 2 || number_of_live_cells == 3))
                 {
-                    table[i, j].futureValue = true;
+                    table[y, x].futureValue = true;
                 }
                 else if (number_of_live_cells == 3)
                 {
-                    table[i, j].futureValue = true;
+                    table[y, x].futureValue = true;
                 }
                 else
                 {
-                    table[i, j].futureValue = false;
+                    table[y, x].futureValue = false;
                 }
             }
+        });
 
         PrepareToNextIteration();
 
