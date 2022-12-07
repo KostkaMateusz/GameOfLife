c267bfc3b11f649a0cd32f0a6b5bf79e45bdf465
Alakeks <mateuszkostka5@gmail.com>
2022-12-07 22:35:52 +0100

    Add gif generation

--- Program.cs
+++ Program.cs
@@ -1,14 +1,35 @@
 ﻿using GameOfLife;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.PixelFormats;
+//using SixLabors.ImageSharp.Formats.Bmp;
+using System.Drawing.Imaging;
+
+int mapXSize = 60;
+int mapYSize = 60;
+int numbersOfRuns =240;
 
 
-
-int mapXSize = 100;
-int mapYSize = 100;
-int numbersOfRuns = 50;
+const int xMapResolution = 600;
+const int yMapResolution = 600;
 
 
-int xMapResolution = 2400;
-int yMapResolution = 2400;
+//Gif
+// Delay between frames in (1/100) of a second.
+const int frameDelay = 10;
+
+// Create empty image.
+using Image<Rgba32> gif = new(xMapResolution, yMapResolution, Color.Black);
+
+// Set animation loop repeat count to 5.
+//var gifMetaData = gif.Metadata.GetGifMetadata();
+//gifMetaData.RepeatCount = 5;
+
+// Set the delay until the next image is displayed.
+GifFrameMetadata metadata = gif.Frames.RootFrame.Metadata.GetGifMetadata();
+metadata.FrameDelay = frameDelay;
+
 
 /*ToDo:
  * Add Grid to HeatMap
@@ -20,22 +41,47 @@ int yMapResolution = 2400;
 var plt = new ScottPlot.Plot(xMapResolution, yMapResolution);
 var gameOfLifeEngine = new GameOfLifeEngine(mapXSize, mapYSize);
 
-gameOfLifeEngine.InsertIntoGame(55, 44, true);
-gameOfLifeEngine.InsertIntoGame(55, 45, true);
-gameOfLifeEngine.InsertIntoGame(55, 46, true);
+gameOfLifeEngine.InsertIntoGame(25, 45, true);
+gameOfLifeEngine.InsertIntoGame(25, 44, true);
+gameOfLifeEngine.InsertIntoGame(25, 46, true);
+                                
+gameOfLifeEngine.InsertIntoGame(23, 45, true);
+gameOfLifeEngine.InsertIntoGame(26, 45, true);
 
-gameOfLifeEngine.InsertIntoGame(53, 45, true);
-gameOfLifeEngine.InsertIntoGame(56, 45, true);
+var ms = new MemoryStream();
+var mapBIt=new System.Drawing.Bitmap(xMapResolution, yMapResolution);
 
+Cell[,]? nextStep;
+double[,]? data2D;
 
-// Generate runs
 for (int k = 0; k < numbersOfRuns; k++)
 {
-    var nextStep=gameOfLifeEngine.CalculateNext();
-    var data2D = GameOfLifeEngine.ConvertToArray(nextStep);    
+    Console.WriteLine(k);
+
+    nextStep=gameOfLifeEngine.CalculateNext();
+    data2D = GameOfLifeEngine.ConvertToArray(nextStep);    
 
     plt.AddHeatmap(data2D);
-    plt.SaveFig($"heatmap_quickstart{k}.png");
+    plt.Render(mapBIt, true,1);
+
+    mapBIt.Save(ms, ImageFormat.Bmp);
+
+    byte[] bitmapData = ms.ToArray();
+
+    var bmpDecoder = new BmpDecoder();
+    using var image = Image.Load(bitmapData, bmpDecoder);
+
+    // Set the delay until the next image is displayed.
+    metadata =image.Frames.RootFrame.Metadata.GetGifMetadata();
+    metadata.FrameDelay = frameDelay;
     
+    // Add the frame to the gif.
+    gif.Frames.AddFrame(image.Frames.RootFrame);    
 }
+
+Console.WriteLine("Zapisuje");
+
+// Save the final result.
+gif.SaveAsGif("output.gif");
+
 
